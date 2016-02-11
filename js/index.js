c963e23d48f3c95a02183ce4a03f50c95915517f
BaptisteAden <baptiste.aden@gmail.com>
2016-02-11 01:28:40 +0100

    Vitrine sympa, code temporaire

--- js/index.js
+++ js/index.js
@@ -25,6 +25,11 @@ $(document).ready(function(){
 		$.each(contribs.personnes, function(keyPersonne, contribsPersonne){
 			$('#tabPersonnes').append( $('<tr>')
 				.append( $('<td>').text(keyPersonne.slice(0,-1)) )
+				//TMP
+				.append( $('<td>') )
+				.append( $('<td>') )
+				.append( $('<td>') )
+				//TMP
 				.append( $('<td>').text(getPercentage(contribsPersonne.corpsMetier)) )
 				.append( $('<td>').text(getPercentage(contribsPersonne.blocGlobal)) )
 				.append( $('<td>').text(getPercentage(contribsPersonne.total)) )
@@ -33,6 +38,9 @@ $(document).ready(function(){
 		$.each(contribs.corpsMetier, function(keyCorpsMetier, contribsCorpsMetier){
 			$('#tabCorpsMetier').append( $('<tr>')
 				.append( $('<td>').text(keyCorpsMetier) )
+				//TMP
+				.append( $('<td>') )
+				//TMP
 				.append( $('<td>').text(getPercentage(contribsCorpsMetier.blocGlobal)) )
 				.append( $('<td>').text(getPercentage(contribsCorpsMetier.total)) )
 			);
@@ -43,6 +51,22 @@ $(document).ready(function(){
 				.append( $('<td>').text(getPercentage(contribsGlobales)) )
 			);
 		});
+		//TMP
+		$.each(json, function(keyGlobal, objGlobal){
+			if(keyGlobal != "contribBrute"){
+				$.each(objGlobal, function(keyCorpsMetier, objCorpsMetier){
+					if(keyCorpsMetier != "contribBrute"){
+						$('#tabCorpsMetier td:nth-child(2):empty').first().text(objCorpsMetier.poids);
+						for(var i=0; i<Object.keys(objCorpsMetier).length-3; i++){	
+							$('#tabPersonnes td:nth-child(2):empty').first().html(objCorpsMetier.metriques.toString().replace(/,/g,"<br>"));
+							$('#tabPersonnes td:nth-child(3):empty').first().html(objCorpsMetier[i].poids.toString().replace(/,/g,"<br>"));
+							$('#tabPersonnes td:nth-child(4):empty').first().html(objCorpsMetier[i].valeurs.toString().replace(/,/g,"<br>"));
+						}
+					}
+				});
+			}
+		});
+		//TMP
 	});
 	
 	//Charger les projets
